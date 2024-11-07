308f5ef880ef4c2d87021e8851cf9c898d810a8a
DavidCaballero <1745463@uab.cat>
2024-11-07 09:26:10 +0100

    Implementacio i edicio inicial

--- premis.sh
+++ premis.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+HM_Funcio ()
+{
+echo -e "\n---------------------------------------------------------\n"
+echo "Catàleg de guanyadors i guanyadores d’Oscars"
+echo -e "\n---------------------------------------------------------\n"
+echo "1 Llistats del catàleg."
+echo "2 Cerca al catàleg."
+echo "3 Com ets d'Expert?."
+echo "4 Gestió del catáleg."
+echo "0 Sortir."
+
+
+
+}
+HM_Funcio BestActor.csv BestActress.csv
