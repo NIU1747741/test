739157201f31515269f72722e2c271e50c3de295
Oscar <antorcha6432@gmail.com>
2024-11-14 09:55:09 +0100

    commit 4 tasca1-2

--- tasca1-2.sh
+++ tasca1-2.sh
@@ -4,7 +4,7 @@ echo "**************************************************************************
 echo -e "******************* 	LLISTAT ALFABETIC DE PEL·LICULES. 	************** \n"
 
 echo -e "*****	 LLISTAT OSCARS A MILLOR ACTOR: \n"
-tail +2 BestActor.csv > temp1
+tail +2 $1 > temp1
 cut -d, -f2-5 temp1 > temp2
 sort -t, -k4 temp2 > temp3
 cat temp3
@@ -14,11 +14,11 @@ rm temp1 temp2 temp3
 echo ""
 
 echo -e "*****	 LLISTAT OSCARS A MILLOR ACTRIU: \n"
-tail +2 BestActress.csv > temp1
+tail +2 $2 > temp1
 cut -d, -f2-5 temp1 > temp2
 sort -t, -k4 temp2 > temp3
 cat temp3
 
 rm temp1 temp2 temp3
 
-echo "************************** PREMI UNA TECLA PER CONTINUAR *************************"
+echo -e "\n ************************** PREMI UNA TECLA PER CONTINUAR *************************"
