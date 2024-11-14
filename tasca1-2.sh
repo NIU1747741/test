b70647693b792712c7daa27552d7f11ff9034d81
Oscar <antorcha6432@gmail.com>
2024-11-14 12:13:50 +0100

    corregit bucle actors, falta actrius

--- tasca1-2.sh
+++ tasca1-2.sh
@@ -14,11 +14,16 @@ lin=`cat temp3 | wc -l`
 
 while [ $i -le $lin ]
 do
-	pel=`head -i $1 | tail -1 | cut -d, -f4`
-	nom=`head -i $1 | tail -1 | cut -d, -f3`
-	edat=`head -i $1 | tail -1 | cut -d, -f2`
-	any=`head -i $1 | tail -1 | cut -d, -f1`
+	pel=`head -$i temp3 | tail -1 | cut -d, -f4`
+	nom=`head -$i temp3 | tail -1 | cut -d, -f3`
+	edat=`head -$i temp3 | tail -1 | cut -d, -f2`
+	any=`head -$i temp3 | tail -1 | cut -d, -f1`
 	
-echo "$pel , guanyador $nom a $any amb $edat anys"
+echo "$pel, guanyador $nom a $any amb $edat anys"
+
+let i=i+1
+done
 
 rm temp1 temp2 temp3
+
+echo ""
