1647df3466f5f487ae8d9a99a75dfcadb81e4710
Oscar <antorcha6432@gmail.com>
2024-11-14 12:07:05 +0100

    introduccio del missatge acabat per actors

--- tasca1-2.sh
+++ tasca1-2.sh
@@ -4,21 +4,21 @@ echo "**************************************************************************
 echo -e "******************* 	LLISTAT ALFABETIC DE PEL·LICULES. 	************** \n"
 
 echo -e "*****	 LLISTAT OSCARS A MILLOR ACTOR: \n"
+i=1
+
 tail +2 $1 > temp1
 cut -d, -f2-5 temp1 > temp2
 sort -t, -k4 temp2 > temp3
-cat temp3
+
+lin=`cat temp3 | wc -l`
+
+while [ $i -le $lin ]
+do
+	pel=`head -i $1 | tail -1 | cut -d, -f4`
+	nom=`head -i $1 | tail -1 | cut -d, -f3`
+	edat=`head -i $1 | tail -1 | cut -d, -f2`
+	any=`head -i $1 | tail -1 | cut -d, -f1`
+	
+echo "$pel , guanyador $nom a $any amb $edat anys"
 
 rm temp1 temp2 temp3
-
-echo ""
-
-echo -e "*****	 LLISTAT OSCARS A MILLOR ACTRIU: \n"
-tail +2 $2 > temp1
-cut -d, -f2-5 temp1 > temp2
-sort -t, -k4 temp2 > temp3
-cat temp3
-
-rm temp1 temp2 temp3
-
-echo -e "\n ************************** PREMI UNA TECLA PER CONTINUAR *************************"
