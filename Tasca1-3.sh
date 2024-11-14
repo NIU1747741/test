93803f801cee518090a4097aa6d7ef2a4df684eb
DavidCaballero <1745463@uab.cat>
2024-11-14 09:44:56 +0100

    Funcio Completada només amb BestActor

--- Tasca1-3.sh
+++ Tasca1-3.sh
@@ -3,9 +3,14 @@
 Tasca1-3 ()
 {
 
-cut -d: -f2- $1 >> temp1 
+cut -d, -f2- $1 > temp1 
+sort -n -t, -k 2 temp1 > OrdenatBestActor
+cat OrdenatBestActor
+rm temp1
+rm OrdenatBestActor
 
-cut -d: -f2- $2 >> temp2
+cut -d, -f2- $2 >> temp2
+rm temp2
 
 }
-Tasca 1-3 $1 $2
+Tasca1-3 $1 $2
