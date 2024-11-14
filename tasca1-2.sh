5cd8bfedf306b521327f76e37dbd19d0bdac7e44
Oscar <antorcha6432@gmail.com>
2024-11-14 12:23:11 +0100

    petites correccions i afegit Actrius finalitzat

--- tasca1-2.sh
+++ tasca1-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "**********************************************************************************"
+echo "******************************************************************************"
 echo -e "******************* 	LLISTAT ALFABETIC DE PEL·LICULES. 	************** \n"
 
 echo -e "*****	 LLISTAT OSCARS A MILLOR ACTOR: \n"
@@ -19,7 +19,35 @@ do
 	edat=`head -$i temp3 | tail -1 | cut -d, -f2`
 	any=`head -$i temp3 | tail -1 | cut -d, -f1`
 	
-echo "$pel, guanyador $nom a $any amb $edat anys"
+echo "De la pelicul·la $pel, guanyador $nom a $any amb $edat anys"
+
+let i=i+1
+done
+
+rm temp1 temp2 temp3
+
+echo ""
+
+echo "******************************************************************************"
+echo -e "******************* 	LLISTAT ALFABETIC DE PEL·LICULES. 	************** \n"
+
+echo -e "*****	 LLISTAT OSCARS A MILLOR ACTRIU: \n"
+i=1
+
+tail +2 $2 > temp1
+cut -d, -f2-5 temp1 > temp2
+sort -t, -k4 temp2 > temp3
+
+lin=`cat temp3 | wc -l`
+
+while [ $i -le $lin ]
+do
+	pel=`head -$i temp3 | tail -1 | cut -d, -f4`
+	nom=`head -$i temp3 | tail -1 | cut -d, -f3`
+	edat=`head -$i temp3 | tail -1 | cut -d, -f2`
+	any=`head -$i temp3 | tail -1 | cut -d, -f1`
+	
+echo "De la pel·licula $pel, guanyadora $nom a $any amb $edat anys"
 
 let i=i+1
 done
