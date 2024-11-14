642f9d32647b59ef79b822ef9db837b36706797e
NIU1747741 <Miguel.GilA@autonoma.cat>
2024-11-14 10:18:48 +0100

    Se ha mejorado la estructura de Opcio-1.sh

--- Opcio-1.sh
+++ Opcio-1.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 
-clear
+mostrarMenu() {
+	clear
+	echo -e "\n---------------------------------------------------------\n"
+	echo -e "1 - Llistats del catàleg"
+	echo -e "\n---------------------------------------------------------\n"
+	echo -e "1. Llistat d'actors i actrius per data del premi."
+	echo -e "2. Llistat alfabètic de pel·lícules."
+	echo -e "3. Llistat per edat d'actors i actrius."
+	echo -e "0. Tornar al menú anterior."
+  echo -e "\nSeleccioneu una opció: "
+}
 
-echo --------------------------------------------------
+# Función para manejar opciones no válidas
+mostrarOpcioNoValida() {
+  clear
+  echo "Opció NO VÀLIDA"
+  sleep 2
+  clear
+}
 
-echo -e "\n \t 1 - Llistats del catàleg. \t \n"
+# Bucle principal del menú
+local intro=""
 
-echo --------------------------------------------------
+while true; do
+	mostrarMenu
+	read intro
 
-echo -e "\n1 Llistat d'actors i actrius per data del premi.\n"
+	case $intro in
+		1)
+			echo "Tasca 1-1 en desenvolupament"
+			;;
+		2)
+			echo "Tasca 1-2 en desenvolupament"
+			;;
+		3)
+			echo "Tasca 1-3 en desenvolupament"
+			;;
+		0)
+			exit
+			;;
+		*)
+			mostrarOpcioNoValida
+			;;
+	esac
+done
 
-echo -e "2 Llistat alfabètic de pel·lícules.\n"
-
-echo -e "3 Llistat per edat d'actors i actrius.\n"
-
-echo -e "0 Tornar al menú anterior."
-
-
-
-read numero
-
-case $numero in
-
-1)
-
-echo "Tasca 1-1 en desenvolupament";;
-
-2)
-
-echo "Tasca 1-2 en desenvolupament";;
-
-3)
-
-echo "Tasca 1-3 en desenvolupament";;
-
-0)
-
-exit;;
-
-*)
-
-echo "Opcio incorrecta";;
-
-esac
+echo ""
 
