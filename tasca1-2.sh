74bb583176dce083fdd60c7e6f8b9b26916c5d06
Oscar <antorcha6432@gmail.com>
2024-11-14 09:45:14 +0100

    commit 2 tasca1-2

--- tasca1-2.sh
+++ tasca1-2.sh
@@ -3,4 +3,9 @@
 echo -e "Llistat alfabètic de pel·licules d'actors nominats a l'OSCAR: \n"
 tail +2 BestActor.csv > temp1
 cut -d, -f2-5 temp1 > temp2
+sort -t, -k4 temp2 > temp3
+cat temp3
 
+rm temp1 temp2 temp3
+
+echo ""
