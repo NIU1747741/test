4e505974cdd36085541c30a1431d35108f43e442
NIU1747741 <Miguel.GilA@autonoma.cat>
2024-11-14 09:42:44 +0100

    Se mustra el listado ordenado

--- tasca1-1.sh
+++ tasca1-1.sh
@@ -9,3 +9,20 @@ fi
 actor_file="$1"
 actress_file="$2"
 
+# Mostrar el listado de premios
+echo "**********************************************************************************"
+echo "******************* LLISTAT D’ACTORS I ACTRIUS PER DATA DEL PREMI. **************"
+
+# Listado de actores
+echo "***** LLISTAT OSCARS A MILLOR ACTOR:"
+tail -n +2 "$actor_file" | awk -F',' '{print "* " $2 ", amb " $3 " anys " $4 " per " $5}' | sort
+
+# Línea de separación
+echo
+
+# Listado de actrices
+echo "***** LLISTAT OSCARS A MILLOR ACTRIU:"
+tail -n +2 "$actress_file" | awk -F',' '{print "* " $2 ", amb " $3 " anys " $4 " per " $5}' | sort
+
+echo "************************** PREMI UNA TECLA PER CONTINUAR *************************"
+read -n 1 -s  # Esperar a que el usuario pulse una tecla para continuar
