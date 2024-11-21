d6413992b74209e4c4f272c4f5921e164b5270f2
DavidCaballero <1745463@uab.cat>
2024-11-21 18:40:41 +0100

    Ordena perfectament, només queda dir si actor o actriu i tornar a menú anterior

--- Tasca2-2.sh
+++ Tasca2-2.sh
@@ -15,15 +15,30 @@ Tasca2-2 ()
         peli=$(echo "$linea" | cut -d, -f5)
         if [[ "${peli,,}" == *"${titol,,}"* ]]; 
         then
-        echo "$linea"
+        echo "$linea" >> filtrar
+        trobat=true
+        fi
+    done
+    
+     for (( i=2; i<=numlineas; i++ )); 
+    do
+        linea=$(head -n $i "$2" | tail -n 1)
+        peli=$(echo "$linea" | cut -d, -f5)
+        if [[ "${peli,,}" == *"${titol,,}"* ]]; 
+        then
+        echo "$linea" >> filtrar
         trobat=true
         fi
     done
     
     if [ "$trobat" = false ]; 
     then
-        echo "Pel·lícula no present al catàleg"
+        echo "Pel·lícula no present al catàleg" 
     fi
-}
 
+sort -k5 -t, filtrar | cut -d, -f2- > filtrar1 
+cat filtrar1
+rm filtrar
+rm filtrar1
+}
 Tasca2-2 "$1" "$2"
