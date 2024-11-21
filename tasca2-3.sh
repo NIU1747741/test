0ea578d3eca1284b6bf6ce3cce41806a86bf5103
NIU1747741 <Miguel.GilA@autonoma.cat>
2024-11-21 09:59:24 +0100

    Se busca los autores y se guardan en un archivo temp

--- tasca2-3.sh
+++ tasca2-3.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # Comprobamos que los archivos BestActor.csv y BestActress.csv se pasen como argumentos
-if [[ $# -ne 2 ]]; then
-  echo "Error: Se requieren dos archivos .csv como argumentos (BestActor.csv y BestActress.csv)"
+if [[ $# -ne 3 ]]; then
+  echo "Error: Se requieren dos archivos .csv como argumentos (BestActor.csv y BestActress.csv) y un string"
   exit 1
 fi
 
 actor_file="$1"
 actress_file="$2"
+search_name="$3"
+
+# Variables temporales para guardar resultados
+temp_file=$(mktemp)
+
+# Buscar coincidencias en el archivo de actores
+tail -n +2 "$actor_file" | awk -F',' -v search="$search_name" '
+tolower($4) ~ tolower(search) {
+  print $4 ", " $2 ", con " $3 " años, premio a MEJOR ACTOR, por " $5
+}' >> "$temp_file"
 
 
-echo "************************** PREMI UNA TECLA PER CONTINUAR *************************"
-read -n 1 -s  # Esperar a que el usuario pulse una tecla para continuar
