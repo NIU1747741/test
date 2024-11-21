33004d2a697b4b7b38dd6e65211b251295423e64
DavidCaballero <1745463@uab.cat>
2024-11-21 10:07:30 +0100

    Funciona Perfectament la búsqueda pero només homes

--- Tasca2-2.sh
+++ Tasca2-2.sh
@@ -4,9 +4,13 @@ Tasca2-2 ()
 {
 echo "Introdueix el títol de la película"
 read titol
-cut -d, -f5 $1 | tail +2 > titols
+cut -d, -f5 "$1" | tail +2 > titols
 
+grep -i "$titol" titols > filtrat
 
+cat filtrat
+
+rm filtrat
 rm titols
 
 }
