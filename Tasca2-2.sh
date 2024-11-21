63c131bf144fbfece2a307e3216e018a8a2464ce
DavidCaballero <1745463@uab.cat>
2024-11-21 10:18:51 +0100

    Me queda por arreglar mucho, el codigo

--- Tasca2-2.sh
+++ Tasca2-2.sh
@@ -8,7 +8,20 @@ cut -d, -f5 "$1" | tail +2 > titols
 
 grep -i "$titol" titols > filtrat
 
-cat filtrat
+let numlineas=wc -l < $1
+
+for (( i=1; i<$numlineas; i++))
+do
+if ((filtrat ~= $valor ));
+then
+cut -d, -f5 "$1" | head -$i | tail -1 > filtrattotal
+cat filtrattotal
+else
+echo "Pel·lícula no present al catàleg"
+fi
+done
+
+
 
 rm filtrat
 rm titols
