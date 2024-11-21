fbdb1bea0b6fad53a98594c4c134630b2dfbfa2c
NIU1747741 <Miguel.GilA@autonoma.cat>
2024-11-21 10:14:12 +0100

    Se añade la opción de volver a buscar o salir

--- tasca2-3.sh
+++ tasca2-3.sh
@@ -31,3 +31,22 @@ echo "****************** RESULTADOS DE LA BÚSQUEDA PARA \"$search_name\" ******
 sort "$temp_file"
 echo "**********************************************************************************"
 
+# Eliminamos el archivo temporal
+rm "$temp_file"
+
+# Opción de volver a buscar o salir
+while true; do
+  echo "¿Quieres realizar otra búsqueda (si/no)?"
+  read -r respuesta
+  if [[ "$respuesta" == "si" ]]; then
+    echo "Introduce una nueva cadena de caracteres para buscar:"
+    read -r new_search_name
+    bash "$0" "$actor_file" "$actress_file" "$new_search_name"
+    break
+  elif [[ "$respuesta" == "no" ]]; then
+    echo "Volviendo al menú anterior."
+    break
+  else
+    echo "Respuesta no válida. Escribe 'si' o 'no'."
+  fi
+done
