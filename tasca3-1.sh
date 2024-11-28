9fa0034f1598e8d32828d7ad23353062d082958a
Oscar Marcos <Oscar.MarcosR@autonoma.cat>
2024-11-28 09:48:25 +0100

    Assegurant que no surten les mateixes pelicules

--- tasca3-1.sh
+++ tasca3-1.sh
@@ -12,8 +12,30 @@ cat Lactors > Tactors
 cat Lactress >> Tactors
 
 #Generamos Película incógnita y las otras 2
-Pel_inc=`sort -R Tactors | head -n 1 | cut -d, -f4-5`
-Pel_rand1=`sort -R Tactors | head -n 1 | cut -d, -f4-5`
-Pel_rand2=`sort -R Tactors | head -n 1 | cut -d, -f4-5`
+Pel_inc=`sort -R Tactors | head -1 | cut -d, -f5`
+Pel_rand1=`sort -R Tactors | head -1 | cut -d, -f5`
+Pel_rand2=`sort -R Tactors | head -1 | cut -d, -f5`
+
+while [ "$Pel_rand1" = "$Pel_inc" ] || [ "$Pel_rand2 = $Pel_inc" ] || [ "$Pel_rand1 = $Pel_rand2" ]
+do
+	if [ $Pel_rand1 = $Pel_inc ] ;
+	then
+		Pel_rand1=`sort -R Tactors | head -1 | cut -d, -f5`
+	fi
+	
+	if [ $Pel_rand2 = $Pel_inc ] ;
+	then
+		Pel_rand2=`sort -R Tactors | head -1 | cut -d, -f5`
+	fi
+	
+	if [ $Pel_rand1 = $Pel_rand2 ] ;
+	then
+		Pel_rand1=`sort -R Tactors | head -1 | cut -d, -f5`
+	fi
+done
+
+echo $Pel_inc
+echo $Pel_rand1
+echo $Pel_rand2
 
 rm Lactors Lactress Tactors
