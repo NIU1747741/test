a9e05ed00bda880d904469d4d5753d28240b803d
DavidCaballero <1745463@uab.cat>
2024-12-01 22:07:00 +0100

    No hem surt millor que això, el problema es que la opció correcta sempre es la primera

--- Tasca3-2.sh
+++ Tasca3-2.sh
@@ -6,19 +6,16 @@ echo -e "-------------------------------------------------- \n"
 
 #HOMES
 
-random=$(echo $(($RANDOM%88)))
-pelirandom=$(cut -d, -f5 $1 | head -$random | tail -1)
-anyrandom=$(cut -d, -f2 $1 | head -$random | tail -1)
-random2=$(echo $(($RANDOM%88)))
-random3=$(echo $(($RANDOM%88)))
+random=$(($RANDOM%88))
+pelirandom=$(awk -F, "NR==$random {print \$5}" $1)
+anyrandom=$(awk -F, "NR==$random {print \$2}" $1)
+random2=$(($RANDOM%88))
+random3=$(($RANDOM%88))
 
 #ACTORSRANDOM
-actor1=$(cut -d, -f4 $1 | head -$random | tail -1)
-echo "2 - " 
-actor2=$(cut -d, -f4 $1 | head -$random2 | tail -1)
-echo "3 - " 
-actor3=$(cut -d, -f4 $1 | head -$random3 | tail -1)
-
+actor1=$(awk -F, "NR==$random {print \$4}" $1)
+actor2=$(awk -F, "NR==$random2 {print \$4}" $1)
+actor3=$(awk -F, "NR==$random3 {print \$4}" $1)
 #PREGUNTA
 
 # He buscat informació, i la millor forma de fer aquesta part, és una barreja amb shuf, si la professora necessita explicació per la meva part de com utilitzar això se la puc donar
@@ -29,7 +26,7 @@ opcions=($(shuf -e "${opcions[@]}"))
 
 for (( i=0; i<${#opcions[@]}; i++))
 do
- if [[ "${opciones[$i]}" == "$actor1" ]]; 
+ if [[ "${opcions[$i]}" == "$actor1" ]]; 
  then
     correcta=$((i + 1))
     break
@@ -37,22 +34,20 @@ do
 done
 
 echo -e "L'oscar a millor actriu amb$pelirandom el va guanyar al$anyrandom \n"
-for (( i=0; i<${#opcions[@]}; i++ ))
-do
-echo " $((i+1)) - ${opciones[$i]}"
-done
+echo "1 - $actor1"  
+echo "2 - $actor2"  
+echo "3 - $actor3"  
 echo -e "\n-------------------------------------------------- \n "
 echo "Selecciona una opció [1,2,3]: "
 
 read Opcio
 
-if [[ $opcio == $correcta ]];
+if [[ $Opcio -eq $correcta ]];
 then
 echo "Felicitats"
 else 
 echo "No felicitats"
 fi
-
 
 #DESPRÉS DONES I RANDOM ENTRE 1 i 2
 
