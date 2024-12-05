a285b81abca71b7825a392e61dadbcb78987b198
NIU1747741 <Miguel.GilA@autonoma.cat>
2024-12-05 09:25:13 +0100

    Se obtiene la linea con el año y mejoras visuales

--- tasca4-3.sh
+++ tasca4-3.sh
@@ -9,6 +9,11 @@ fi
 actor_file="$1"
 actress_file="$2"
 
+esperarPulsarTecla() {
+	echo "************************** PREMI UNA TECLA PER CONTINUAR *************************"
+	read -n 1 -s  # Esperar a que el usuario pulse una tecla para continuar
+}
+
 # Preguntar si se quiere modificar actor o actriz
 echo "¿Quieres modificar un actor o una actriu? (actor/actriu)"
 read tipo
@@ -19,10 +24,20 @@ elif [[ "$tipo" == "actriu" ]]; then
   selected_file="$actress_file"
 else
   echo "Error: Opción no válida. Debes escribir 'actor' o 'actriu'."
+  esperarPulsarTecla
   exit 1
 fi
 
+# Pedir el año como campo obligatorio
+echo "Introdueix l'any de la pel·lícula que vols modificar:"
+read year
 
+# Buscar la línea que contiene el año indicado
+line=$(grep -m 1 "^.*, *$year,.*" "$selected_file")
+if [[ -z "$line" ]]; then
+  echo "Error: No s'ha trobat cap registre amb l'any $year."
+  esperarPulsarTecla
+  exit 1
+fi
 
-echo "************************** PREMI UNA TECLA PER CONTINUAR *************************"
-read -n 1 -s  # Esperar a que el usuario pulse una tecla para continuar
+esperarPulsarTecla
