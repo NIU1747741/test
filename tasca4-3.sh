e36a1171f52a446774c9f81fc9ac51a2bac20f1f
NIU1747741 <Miguel.GilA@autonoma.cat>
2024-12-05 08:57:23 +0100

    Se obtiene el fichero que se quiere modificar

--- tasca4-3.sh
+++ tasca4-3.sh
@@ -9,3 +9,16 @@ fi
 actor_file="$1"
 actress_file="$2"
 
+# Preguntar si se quiere modificar actor o actriz
+echo "¿Quieres modificar un actor o una actriu? (actor/actriu)"
+read tipo
+
+if [[ "$tipo" == "actor" ]]; then
+  selected_file="$actor_file"
+elif [[ "$tipo" == "actriu" ]]; then
+  selected_file="$actress_file"
+else
+  echo "Error: Opción no válida. Debes escribir 'actor' o 'actriu'."
+  exit 1
+fi
+
