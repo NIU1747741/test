e83537a1f63cb86ddf48ac4ae201e5ba50444782
DavidCaballero <1745463@uab.cat>
2024-12-05 16:20:36 +0100

    Creat un arxiu per fer proves i funciona el programa (falta fer dones i que pugi afegir al document)

---
+++ tasca4-1.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+BESTACTOR=$1
+BESTACTRESS=$2
+
+echo "Que vols afegir?:"
+echo "1 - Actor"
+echo "2 - Actriu"
+
+read actoroactriu
+
+if [[ $actoroactriu -eq 1 ]];
+then
+echo "Dona l'any"
+read any
+echo "Dona el títol"
+read titol
+echo "Dona el nom"
+read nom
+echo "Dona l'edat"
+read edat
+
+awk -F, 'BEGIN {}
+$2 == a {print "true" > tof}
+END {}' a=$any tof="tof" $BESTACTOR
+
+if [[ 'cat tof' == true ]];
+then
+echo "Any repetit"
+rm tof
+else
+numlineas=$(wc -l < $BESTACTOR)
+numlineas=$((numlineas+2))
+echo $numlineas > dlineas
+echo $any > dany
+echo $titol > dtitol
+echo $nom > dnom
+echo $edat > dedat
+echo " " > hueco 
+paste -d, dlineas dany dedat dnom dtitol
+
+fi
+
+#mujeres
+else
+echo "Hola"
+fi
+
+
