c48a550dae91bf8096cc0aae8a6946e6d0fd8993
DavidCaballero <1745463@uab.cat>
2024-12-05 21:15:45 +0100

    Esteticament es igual, pero ara si es repteix l'any segueix posant la peli

--- tasca4-1.sh
+++ tasca4-1.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
 BESTACTOR=$1
-BESTACTRESS=$2
-
-echo "Que vols afegir?:"
-echo "1 - Actor"
-echo "2 - Actriu"
-
-read actoroactriu
-
-if [[ $actoroactriu -eq 1 ]];
-then
-echo "Dona l'any"
-read any
-echo "Dona el títol"
-read titol
-echo "Dona el nom"
-read nom
-echo "Dona l'edat"
-read edat
+any=$2
+titol=$3
+nom=$4
+edat=$5
 
 awk -F, 'BEGIN {}
 $2 == a {print "true" > tof}
@@ -31,57 +17,12 @@ rm tof
 else
 numlineas=$(wc -l < $BESTACTOR)
 numlineas=$((numlineas+2))
-echo $numlineas > dlineas
-echo $any > dany
-echo $titol > dtitol
-echo $nom > dnom
-echo $edat > dedat
 
-paste -d, dlineas dany dedat dnom dtitol >> $BESTACTOR
-rm dlineas
-rm dany
-rm dnom
-rm dedat
-rm dtitol
+#he fet millor per variables, ja que amb els arxius era poc òptim y no es podia separar correctament
+
+nueva_linea="$numlineas, $any, $edat, \"$nom\", \"$titol\""
+echo "$nueva_linea" >> "$BESTACTOR"
+
 
 fi
-
-#mujeres
-else
-echo "Dona l'any"
-read any
-echo "Dona el títol"
-read titol
-echo "Dona el nom"
-read nom
-echo "Dona l'edat"
-read edat
-
-awk -F, 'BEGIN {}
-$2 == a {print "true" > tof}
-END {}' a=$any tof="tof" $BESTACTRESS
-
-if [[ 'cat tof' == true ]];
-then
-echo "Any repetit"
-rm tof
-else
-numlineas=$(wc -l < $BESTACTRESS)
-numlineas=$((numlineas+2))
-echo $numlineas > dlineas
-echo $any > dany
-echo $titol > dtitol
-echo $nom > dnom
-echo $edat > dedat
-
-paste -d, dlineas dany dedat dnom dtitol >> $BESTACTRESS
-rm dlineas
-rm dany
-rm dnom
-rm dedat
-rm dtitol
-
-fi
-fi
-
 
