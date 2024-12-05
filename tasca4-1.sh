e3a81750e960a738be245138d1c7c85c61d723ea
DavidCaballero <1745463@uab.cat>
2024-12-05 20:50:36 +0100

    Funciona amb homes i dones, falta separar

--- tasca4-1.sh
+++ tasca4-1.sh
@@ -48,7 +48,40 @@ fi
 
 #mujeres
 else
-echo "Hola"
+echo "Dona l'any"
+read any
+echo "Dona el títol"
+read titol
+echo "Dona el nom"
+read nom
+echo "Dona l'edat"
+read edat
+
+awk -F, 'BEGIN {}
+$2 == a {print "true" > tof}
+END {}' a=$any tof="tof" $BESTACTRESS
+
+if [[ 'cat tof' == true ]];
+then
+echo "Any repetit"
+rm tof
+else
+numlineas=$(wc -l < $BESTACTRESS)
+numlineas=$((numlineas+2))
+echo $numlineas > dlineas
+echo $any > dany
+echo $titol > dtitol
+echo $nom > dnom
+echo $edat > dedat
+
+paste -d, dlineas dany dedat dnom dtitol >> $BESTACTRESS
+rm dlineas
+rm dany
+rm dnom
+rm dedat
+rm dtitol
+
+fi
 fi
 
 
